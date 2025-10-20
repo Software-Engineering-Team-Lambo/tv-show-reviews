942c2b19dc1209fa7e9f3a0b1b642bf0121678f8
Joseph Charles <30187981+j027@users.noreply.github.com>
2025-10-20 16:57:13 -0400

    attempt to setup automatic deployment

--- apps/api/src/routes/root.ts
+++ apps/api/src/routes/root.ts
@@ -4,6 +4,10 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
     return { root: true }
   })
+
+  fastify.get('/health', async function (request, reply) {
+    return { status: 'ok', timestamp: new Date().toISOString() }
+  })
 }
 
 export default root
