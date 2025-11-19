8ee767aae890066cc58adfd5f42f84d59a45364f
kevboo3 <kevink6@umbc.edu>
2025-11-19 17:21:50 -0500

    added profile.ts file

---
+++ apps/api/src/routes/profile.ts
@@ -0,0 +1,6 @@
+//pull user data from login to display on profile page
+import fastify, { FastifyPluginAsync } from "fastify";
+
+//const user: FastifyPluginAsync = async (fastify) => {}
+
+//fastify.get("/api/user",{}, async (request, reply) => { return request.user; });
