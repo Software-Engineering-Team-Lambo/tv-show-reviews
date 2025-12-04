bb823db18c4e11523682286cf44499afedab72aa
Joseph Charles <30187981+j027@users.noreply.github.com>
2025-12-04 23:26:49 +0000

    adjust e2e tests to run a little faster

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -12,8 +12,6 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI */
-  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use */
   reporter: process.env.CI ? 'github' : [['html', { open: 'never' }]],
   /* Shared settings for all the projects below */
